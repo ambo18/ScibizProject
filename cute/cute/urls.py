b95b011a935e32c876551a6b0a46601cf9db342e
cuteyanyanie <nyannemul@gmail.com>
2024-04-11 13:16:36 +0800

    first comment for project urls

--- cute/cute/urls.py
+++ cute/cute/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import MapView  # Import your map view
 
 urlpatterns = [
+    # project urls
     path('map/', MapView.as_view(), name='map'),  # Define URL pattern for map view
     # Add other URL patterns as needed
 ]
